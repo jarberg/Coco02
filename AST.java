b8d91018772f49e21d64481dfd048bbc7cc36939
jarberg <mjarberg88@gmail.com>
2019-11-13 11:37:53 +0100

    task 01 complette

--- AST.java
+++ AST.java
@@ -15,23 +15,58 @@ abstract class AST{
 
 class Start extends AST{
     public List<DataTypeDef> datatypedefs;
+    StringBuilder string = new StringBuilder();
+
     Start(List<DataTypeDef> datatypedefs){
-	this.datatypedefs=datatypedefs;
+        this.datatypedefs=datatypedefs;
+        string.append("\n");
+        string.append("import java.util.*;\n");
+        string.append("abstract class AST{}\n\n");
     }
+
     public String translate(){
-	return "";
-	// You may start here by replacing this with something else
+
+        for(DataTypeDef data : datatypedefs ){
+            string.append(data.translate());
+        }
+        string.append("\n");
+
+        return string.toString();
     }
 }
 
 class DataTypeDef extends AST{
+
     public String dataTypeName;
     public String functionHead;
     public List<Alternative> alternatives;
+    public String type;
     DataTypeDef(String dataTypeName, String functionHead, List<Alternative> alternatives){
 	this.dataTypeName=dataTypeName;
 	this.functionHead=functionHead;
 	this.alternatives=alternatives;
+    }
+    StringBuilder string = new StringBuilder();
+
+    public String translate(){
+
+        string.append("abstract class "+dataTypeName+" extends AST");
+        string.append("{\n");
+        string.append("public abstract");
+        string.append(functionHead);
+        string.append(";\n}");
+
+        string.append("\n");
+        for (Alternative alt : alternatives ) {
+            string.append("\n");
+            string.append("class ");
+            alt.type = dataTypeName;
+            alt.funchead = functionHead;
+            string.append(alt.translate());
+            string.append("\n");
+        }
+        string.append("\n");
+        return string.toString();
     }
 }
 
@@ -39,15 +74,57 @@ class Alternative extends AST{
     public String constructor;
     public List<Argument> arguments;
     public String code;
+    public String type;
+    public String funchead;
     Alternative(String constructor, List<Argument> arguments,  String code){
 	this.constructor=constructor;
 	this.arguments=arguments;
 	this.code=code;
+    }
+    StringBuilder string = new StringBuilder();
+    public String translate(){
+
+        string.append(constructor);
+        string.append(" extends "+type);
+        string.append("{\n");
+        for (Argument arg : arguments ) {
+            string.append("   ");
+            string.append(arg.translate());
+        }
+        string.append("   "+constructor);
+        string.append("( ");
+
+        for (Argument arg : arguments ) {
+            string.append(arg.type+" "+arg.name+", ");
+        }
+        string.deleteCharAt(string.length()-2);
+        string.append(")");
+        string.append("{ \n");
+        for (Argument arg : arguments ) {
+            string.append("   this.").append(arg.name).append(" = ").append(arg.name).append(";\n");
+        }
+        string.append("   }");
+
+        string.append("\n");
+        string.append("\n");
+        string.append("  public"+funchead);
+        string.append(code);
+        string.append("\n");
+        string.append("}");
+        return string.toString();
     }
 }
 
 class Argument extends AST{
     public String type;
     public String name;
+    StringBuilder string = new StringBuilder();
     Argument(String type, String name){this.type=type; this.name=name;}
+    public String translate(){
+        string.append(type);
+        string.append(" ");
+        string.append(name+";");
+        string.append("\n");
+        return string.toString();
+    }
 }
